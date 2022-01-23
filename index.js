84c6ac2cd1a7f9666e54286fb37bd42b9d904283
Himanshu Sharma <himanshu0719.cse19@chitkara.edu.in>
2022-01-23 16:13:45 +0530

    Added the functionality of add and delete items in the list

--- index.js
+++ index.js
@@ -16,8 +16,16 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.get('/',function(req,res){
-    return res.render('todo',{
-        title:'Todo app'
+    Todo.find({},function(err,todo){
+        if(err)
+        {
+            console.log("error detected");
+            return;
+        }
+        return res.render('todo',{
+            title:'ToDo app',
+            todo:todo
+        })
     })
 })
 
@@ -34,6 +42,16 @@ app.post('/enter-data',function(req,res){
             return;
         }
         res.redirect('back');
+    })
+})
+app.get('/delete-item',function(req,res){
+    console.log(req.query.id)
+    Todo.findByIdAndDelete(req.query.id,function(err){
+        if(err){
+            console.log("error occured");
+            return;
+        }
+        return res.redirect('back')
     })
 })
 
